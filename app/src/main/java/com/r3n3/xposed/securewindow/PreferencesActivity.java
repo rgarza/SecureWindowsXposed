3f82d68368892032e4e955f4196eaac1036ddf84
Rene de la Garza <rene.delagarza@gmail.com>
2014-07-13 16:46:27 -0500

    remove menu

--- app/src/main/java/com/r3n3/xposed/securewindow/PreferencesActivity.java
+++ app/src/main/java/com/r3n3/xposed/securewindow/PreferencesActivity.java
@@ -61,25 +61,6 @@ public class PreferencesActivity extends ListActivity {
         return preferences.getStringSet(SECURE_APPS, null);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.preferences, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void getInstalledApplications() {
 
         installedApplications = new ArrayList<PreferencesModel>();
